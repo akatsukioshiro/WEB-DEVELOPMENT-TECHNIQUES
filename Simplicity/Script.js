e007e36d3660cdc7de2da89e7708f5f572e3b5db
Akatsuki Oshiro ~ Ash <ashishnair.oph@gmail.com>
2019-03-26 12:45:48 +0530

    Simple Text to create HTML Pages
    Currently can create divs

--- Simplicity/Script.js
+++ Simplicity/Script.js
@@ -1,52 +1,51 @@
-//var create=document.querySelectorAll('[create]').tagName;
-var x = document.getElementsByTagName("create")[0].childNodes;
-
-var txt,fText,res;
-for(var i = 0; i < x.length; i++) {
-    txt = txt + x[i].nodeName + "<br>";
-	if (x[i].nodeName === "#text") {
-        fText = x[i].nodeValue;
-    }
-  }
-fText = fText.replace(/\r?\n|\r/g, " ");
-res =fText.split(" ");
-var elem;
-for(var i=0;i<res.length;i++)
+var count=document.getElementsByTagName("create").length;
+for(var e=0;e<count;e++)
 {
-	if(res[i]==="create")
-	{	
-		elem=document.createElement(res[i+1]);
-	}
-	if(res[i]==="dimensions")
+	var x = document.getElementsByTagName("create")[0].childNodes;
+	var txt,fText,res;
+	for(var i = 0; i < x.length; i++) 
 	{
-		var hw=res[i+1].split("*");
-		elem.style.height=hw[0]+"vh";
-		elem.style.width=hw[1]+"vw";
-	}
-	if(res[i]==="color")
-	{
-		elem.style.backgroundColor=res[i+1];
-		var c = document.body.children;
-		var ctxt = "";
-  		for (var j = 0; j < c.length; j++) 
+    		if (x[i].nodeName === "#text") 
 		{
-    			ctxt=c[j].tagName.toLowerCase();
-			if(ctxt==="create")
-			{
-				var dd=document.getElementsByTagName(ctxt)[0];
-				dd.parentNode.removeChild(dd);
-				//alert("1");					
+        		fText = x[i].nodeValue;
+	    	}
+	}
+	fText = fText.replace(/\r?\n|\r/g, " ");
+	res =fText.split(" ");
+	var elem;
+	for(var i=0;i<res.length;i++)
+		{
+			if(res[i]==="create")
+			{	
+				elem=document.createElement(res[i+1]);
 			}
-  		}
-	}
-	if((res.length-i)===1)
-	{
-		document.body.appendChild(elem);
-		var c= document.body.children; 
+			if(res[i]==="dimensions")
+			{
+				var hw=res[i+1].split("*");
+				elem.style.height=hw[0]+"vh";
+				elem.style.width=hw[1]+"vw";
+				elem.style.display="block";
+			}
+			if(res[i]==="color")
+			{
+				elem.style.backgroundColor=res[i+1];
+			}
+			if((res.length-i)===1)
+			{
+				document.body.appendChild(elem);
+				var c = document.body.children;
+				var ctxt = "";
+  				for (var j = 0; j < c.length; j++) 
+				{
+    					ctxt=c[j].tagName.toLowerCase();
+					if(ctxt==="create")
+					{
+						var dd=document.getElementsByTagName(ctxt)[0];
+						dd.parentNode.removeChild(dd);
+					}
+	  			}
 		
-	}
-} 
-//txt=txt+res[i]+"<br>"
-//document.body.innerHTML = txt;
-//document.body.innerHTML = txt+"<br>"+x.length;
-//alert(res[0]);
+			}
+
+		}
+}
